b6780e076a3e239a232b027adc5bafc452f7a0c6
xookerchang <e84991211@gmail.com>
2020-11-07 15:39:56 +0800

    calculate average lenght of each comment

--- read.py
+++ read.py
@@ -8,6 +8,12 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
-print(data[0])
-print('-------------------------')
-print(data[1])
+print('檔案讀取完了, 總共有', len(data),'筆資料')
+
+
+#--calculate the average lenght of each comment
+
+sum_com = 0
+for d in data:
+	sum_com += len(d)
+print('每個留言的平均程度為', sum_com/len(data))
