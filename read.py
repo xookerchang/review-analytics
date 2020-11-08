e595f0dc852d6eefc2e05a97942646a67c748ef5
xookerchang <e84991211@gmail.com>
2020-11-08 11:07:57 +0800

    add filtering fuction

--- read.py
+++ read.py
@@ -17,3 +17,13 @@ sum_com = 0
 for d in data:
 	sum_com += len(d)
 print('每個留言的平均程度為', sum_com/len(data))
+
+#--adding filtering functon 
+new = []
+for c in data:
+	if len(c) < 100:
+		new.append(c)
+print('一共有', len(c), '留言長度小於100')
+print('------------------------------')
+print(new[0])
+print(new[1])
